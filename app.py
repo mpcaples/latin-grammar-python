8e6d060a94ce1f21cb1dfeed4929a6453b542f80
madelinepc <madeline@fuegoleads.io>
2024-06-10 12:32:57 -0400

    fixed cors error?

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, jsonify 
+from flask_cors import CORS 
 from grammar import generate_sentence
 
 app = Flask(__name__)
+CORS(app) 
 
 @app.route("/")
 def sentence():
